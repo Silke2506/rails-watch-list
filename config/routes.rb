f27530281aca0e4b4b1e1dc4845cb2b5273a8114
Silke Guthrie <silke.guthrie@gmail.com>
2021-05-20 16:48:25 +0200

    views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+
+  resources :lists, only: [:index, :show, :new, :create]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# delete '/restaurants/:id', to: 'restaurants#destroy'
+#     get '/restaurants', to: 'restaurants#index'
+#     get '/restaurants/:id', to: 'restaurants#show'
+#     get '/restaurants/:id/edit', to: 'restaurants#edit'
+#     get '/restaurants/new', to: 'restaurants#new'
+#     patch '/restaurants/:id', to: 'restaurants#update'
+#     post '/restaurants', to: 'restaurants#create'
