483ede4e47b54b68ff0edb23d94949c2cfe9b04c
Silke Guthrie <silke.guthrie@gmail.com>
2021-05-21 15:49:25 +0200

    finalised bookmarks controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,38 @@
 class BookmarksController < ApplicationController
 
+  def index
+    @bookmark = Bookmark.all
+  end
+
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    if @boomark.save
-      redirect_to list_bookmarks_path(@list)
+    @bookmark.list = @list
+    @bookmark.list_id = @list.id
+    if @bookmark.save
+      redirect_to list_path(@list)
     else
       render :new
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
 
   private
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
   end
+
 
 end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   has_many :bookmarks
+  has_many :lists, through: :bookmarks
   validates :overview, presence: true
 
 end
